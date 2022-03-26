3dc4b4b33daa76e6363ccf088035a3b9e75876fc
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-03-25 23:33:35 -0400

    added accessors and mutators

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -5,4 +5,67 @@ public class Sneaker {
     private String name, brand, sport, color;
     private float size, price;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getSize() {
+        return size;
+    }
+
+    public void setSize(float size) {
+        this.size = size;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
--- src/test/java/models/SneakerTest.java
+++ src/test/java/models/SneakerTest.java
@@ -1,7 +1,126 @@
 package models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SneakerTest {
+
+    @Test
+    void getIdTest() {
+        // G
+        int expected = 3289;
+
+        // W
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setId(expected);
+        testSneaker.getId();
+
+        // T
+        Assertions.assertEquals(expected, testSneaker.getId());
+    }
+
+
+    @Test
+    void getQtyTest() {
+        // G
+        int expected = 2;
+
+        // W
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setQty(expected);
+        testSneaker.getQty();
+
+        // T
+        Assertions.assertEquals(expected, testSneaker.getQty());
+    }
+
+
+    @Test
+    void getNameTest() {
+        // G
+        String expected = "DrewHouse";
+
+        // W
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setName(expected);
+        testSneaker.getName();
+
+        // T
+        Assertions.assertEquals(expected, testSneaker.getName());
+    }
+
+    @Test
+    void getBrandTest() {
+        // G
+        String expected = "Crocs";
+
+        // W
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setBrand(expected);
+        testSneaker.getBrand();
+
+        // T
+        Assertions.assertEquals(expected, testSneaker.getBrand());
+    }
+
+
+    @Test
+    void getSportTest() {
+        // G
+        String expected = "Leisure";
+
+        // W
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setSport(expected);
+        testSneaker.getSport();
+
+        // T
+        Assertions.assertEquals(expected, testSneaker.getSport());
+    }
+
+
+    @Test
+    void getColorTest() {
+        // G
+        String expected = "Yellow";
+
+        // W
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setColor(expected);
+        testSneaker.getColor();
+
+        // T
+        Assertions.assertEquals(expected, testSneaker.getColor());
+    }
+
+    @Test
+    void getSizeTest() {
+        // G
+        float expected = 9.50f;
+
+        // W
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setSize(expected);
+        testSneaker.getSize();
+
+        // T
+        Assertions.assertEquals(expected, testSneaker.getSize());
+    }
+
+    @Test
+    void getPriceTest() {
+        // G
+        float expected = 70f;
+
+        // W
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setPrice(expected);
+        testSneaker.getPrice();
+
+        // T
+        Assertions.assertEquals(expected, testSneaker.getPrice());
+    }
 
 }
